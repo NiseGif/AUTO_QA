c0123adb409bac546efab2383de35d768fd606f4
NiseGif <aaa06062004@gmail.com>
2026-03-03 18:15:41 +0300

    Add class RegisterPageLocators

--- page/locators.py
+++ page/locators.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
 
 class ProductLocators:
     ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
@@ -16,14 +18,24 @@ class ProductLocators:
     PRICE_BOOK = (By.CSS_SELECTOR, '.product_main .price_color')
     PRICE_BOOK_IN_BASKET = (By.CSS_SELECTOR, '.basket-mini')
 
+
 class BasePageLocators:
-    BASKET = (By.CSS_SELECTOR,"span.btn-group")
+    BASKET = (By.CSS_SELECTOR, "span.btn-group")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     LANGUAGE = (By.CSS_SELECTOR, "[name='language']")
     BUTTION_LANGUAGE = (By.CSS_SELECTOR, "button[type = 'submit']")
     WEB_LANGUAGE = (By.CSS_SELECTOR, "html")
 
+
 class BasketPageLocators:
-    PRODUCT_FIELD = (By.CSS_SELECTOR,"#basket_formset") #Поле товара
+    PRODUCT_FIELD = (By.CSS_SELECTOR, "#basket_formset")  # Поле товара
     TEXT_NULL_BASKET = (By.CSS_SELECTOR, "div#content_inner p")
+
+
+class RegisterPageLocators:
+    INPUT_EMAIL = (By.ID, "id_login-username")
+    INPUT_PASSWORD = (By.ID, "id_login-password")
+    INPUT_REGISTER_EMAIL = (By.ID, "id_registration-email")
+    INPUT_REGISTER_PASSWORD = (By.ID, "id_registration-password1")
+    INPUT_REGISTER_PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
